51af43dd3fbe9f8f8fd805921a2d495a9af5a875
felicengie <felice.chandra@stonybrook.edu>
2025-02-22 16:44:24 -0500

    updated pages

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import '../App.css'; // Ensure your CSS file is imported
 
 function Home() {
@@ -16,7 +17,9 @@ function Home() {
       {/* Content of the homepage */}
       <div className="container text-center mt-5">
         <h1>Welcome to the Food Guessing Game</h1>
-        <Button variant="primary">Play Game</Button>
+        <Link to="/BigBack">
+          <Button variant="primary">Play Game</Button>
+        </Link>
       </div>
     </div>
   );
