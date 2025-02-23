4ded0ec7415d5d7c49264711ead412c69cb6115a
felicengie <felice.chandra@stonybrook.edu>
2025-02-23 01:04:47 -0500

    treasure chest

---
+++ client/src/components/treasureChest.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AnimatedGif() {
+  return (
+    <div>
+       <div className="chest-container"></div>;
+    </div>
+  );
+}
+
+export default AnimatedGif;
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import '../App.css'; // Ensure your CSS file is imported
-
+import AnimatedGif from "../components/treasureChest";
 
 function Home() {
+  const [isStarted, setIsStarted] = useState(false);
+  const handleSetSailClick = () => {
+    setIsStarted(!isStarted);
+  };
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true); // This will make the chest visible after a certain time
+    }, 1000); // 2000ms, adjust this based on your animation timing
+
+    return () => clearTimeout(timer); // Cleanup timeout on unmount
+  }, [isStarted]);
+
   return (
     <div className="home-container">
       {/* Static Wave */}
       <div className="wave-static"></div>
+      
       
       {/* Waves Background */}
 
@@ -25,11 +40,23 @@ function Home() {
      
       {/* Content of the homepage */}
       <div className="container text-center mt-5">
-        <h1>Big Back Voyage</h1>
-        <Link to="/BigBack">
-          <Button className="set-sail">Set Sail</Button>
-        </Link>
-         {/* Ship Image */}
+          <h1>Big Back Voyage</h1>
+          <Link to="/BigBack">
+            <Button className="set-sail">Set Sail</Button>
+          </Link>
+          <Button onClick={handleSetSailClick}>Hello</Button>
+          {/* Render animation and island image after button click */}
+       
+          {isStarted && (
+            <>
+             
+                  <div className="right-side-container">
+                  <img src="island.png" alt="Island" className="island-image" />
+                  <div className={`chest-container ${isVisible ? 'show' : ''}`}></div>
+                </div>
+            </>
+          )}
+
        
 
       </div>
