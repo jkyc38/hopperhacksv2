50f41757360f71ba41f7ce3d21689cd359d95297
Jacky <thejackychen@gmail.com>
2025-02-22 21:47:37 -0500

    backend

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage";
 import GamePage from "./pages/GamePage";
 import Test from "./pages/TestPage";
-
+import axios from "axios"
 
 function App() {
   const [cuisineScore, setCuisineScore] = useState(0)
@@ -25,18 +25,18 @@ function App() {
     fetchData(); // Call the async function
   }, []); // Empty dependency array means it runs once on mount
 
-  async function sendCuisineScores(scores){
-    const scoreData = {
-      "Chinese": 4,
-      "Korean":2.1,
-      "Japanese": 3
-    }
-    const ENDPOINT = "ENDPOINT";
-    const res = axios.get(ENDPOINT, {
-      params: scoreData
-    });
+  // async function sendCuisineScores(scores){
+  //   const scoreData = {
+  //     "Chinese": 4,
+  //     "Korean":2.1,
+  //     "Japanese": 3
+  //   }
+  //   const ENDPOINT = "ENDPOINT";
+  //   const res = axios.get(ENDPOINT, {
+  //     params: scoreData
+  //   });
     
-  }
+  // }
   return (
     <Router>
       <Routes>
